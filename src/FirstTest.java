b9267d50fa1e1857e9d6c8b3b5d24e6d500347bd
Gavrilova <gavrilova.irina@gmail.com>
2018-05-29 11:03:44 -0500

    Added find, click, sendkeys, wait methods
    To FirstTest class added find, click, send keys, wait and overloaded
    methods.

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -2,9 +2,14 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 /**
  * Created by irinagavrilova on 5/17/18.
@@ -33,6 +38,25 @@ public class FirstTest {
 
   @Test
   public void FirstTest() {
+    WebElement element_to_init_search = driver.findElement(By.xpath("//*[contains(@text,'Search Wikipedia')]"));
+    element_to_init_search.click();
+    WebElement element_to_enter_search_line = waitElementByXPath(
+            "//*[contains(@text,'Search…')]",
+            "Cannot find search input");
+    element_to_enter_search_line.sendKeys("Appium");
     System.out.println("First test runs");
   }
+
+  private WebElement waitElementByXPath(String xpath, String error_message, long timeInSeconds) {
+    WebDriverWait wait = new WebDriverWait(driver, timeInSeconds);
+    wait.withMessage(error_message + "\n");
+    By by = By.xpath(xpath);
+    return wait.until(presenceOfElementLocated(by));
+  }
+
+  private WebElement waitElementByXPath(String xpath, String error_message) {
+    return waitElementByXPath(xpath, error_message, 5);
+  }
 }
+
+
