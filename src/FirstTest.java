5fbbadaf1105501bee4f420c537a2655cfae7746
Gavrilova <gavrilova.irina@gmail.com>
2018-05-30 18:33:13 -0500

    Added testCancelSearch() class
    Added testCancelSearch() class to cancel search in Wikipedia app;

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,6 +1,7 @@
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -9,6 +10,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.invisibilityOfElementLocated;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 /**
@@ -36,6 +38,7 @@ public class FirstTest {
     driver.quit();
   }
 
+  @Ignore
   @Test
   public void FirstTest() {
     waitForElementByXpathAndClick(
@@ -48,32 +51,69 @@ public class FirstTest {
             "Cannot find search input",
             5);
 
-    waitElementByXPath(
+    waitElementPresentByXPath(
             "//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']",
             "Cannot find 'Object-oriented programming language' text in topuc searching by 'Java'",
             15);
   }
 
-  private WebElement waitElementByXPath(String xpath, String error_message, long timeInSeconds) {
+  @Test
+  public void testCancelSearch() {
+
+    waitForElementByXIdAndClick(
+            "org.wikipedia:id/search_container",
+            "Cannot find 'Search Wikipedia' input",
+            5);
+    waitForElementByXIdAndClick(
+            "org.wikipedia:id/search_close_btn",
+            "Cannot find 'X to cancel search",
+            5
+    );
+    waitForElementNotPresent(
+            "org.wikipedia:id/search_close_btn",
+            "'X' is still present on the page",
+            5
+    );
+  }
+
+  private WebElement waitElementPresentByID(String id, String error_message, long timeInSeconds) {
+    WebDriverWait wait = new WebDriverWait(driver, timeInSeconds);
+    wait.withMessage(error_message + "\n");
+    By by = By.id(id);
+    return wait.until(presenceOfElementLocated(by));
+  }
+  private WebElement waitForElementByXIdAndClick(String id, String error_message, long timeoutInSeconds) {
+    WebElement element = waitElementPresentByID(id, error_message, timeoutInSeconds);
+    element.click();
+    return element;
+  }
+
+  private boolean waitForElementNotPresent(String id, String error_message, long timeInSeconds){
+    WebDriverWait wait = new WebDriverWait(driver, timeInSeconds);
+    wait.withMessage(error_message + "\n");
+    By by = By.id(id);
+    return wait.until(invisibilityOfElementLocated(by));
+  }
+
+  private WebElement waitElementPresentByXPath(String xpath, String error_message, long timeInSeconds) {
     WebDriverWait wait = new WebDriverWait(driver, timeInSeconds);
     wait.withMessage(error_message + "\n");
     By by = By.xpath(xpath);
     return wait.until(presenceOfElementLocated(by));
   }
 
-  private WebElement waitElementByXPath(String xpath, String error_message) {
-    return waitElementByXPath(xpath, error_message, 5);
+  private WebElement waitElementPresentByXPath(String xpath, String error_message) {
+    return waitElementPresentByXPath(xpath, error_message, 5);
   }
 
   private WebElement waitForElementByXpathAndClick(String xpath, String error_message, long timeoutInSeconds) {
-    //wait for present of element and after click on it
-    WebElement element = waitElementByXPath(xpath, error_message, timeoutInSeconds);
+    WebElement element = waitElementPresentByXPath(xpath, error_message, timeoutInSeconds);
     element.click();
     return element;
   }
 
   private WebElement waitForElementByXpathAndSendKeys(String xpath, String value, String error_message, long timeoutInSeconds) {
-    WebElement element = waitElementByXPath(xpath, error_message, timeoutInSeconds);
+    WebElement element = waitElementPresentByXPath(xpath, error_message, timeoutInSeconds);
     element.sendKeys(value);
     return element;
   }
